76c2870905ffffe6760dde3058cd8b150091385b
James Finlinson <the30clues@gmail.com>
2025-12-26 14:08:44 -0700

    Introduce BigShading for displaying dots

--- resources.py
+++ resources.py
@@ -46,7 +46,7 @@ COLOR_NAMES = defaultdict(lambda: "Unrecognized", {
 
 class BigChar:
   _width: int
-  _height = 7
+  _height: int
   _lines: str
 
 
@@ -60,27 +60,33 @@ class BigChar:
 
   @staticmethod
   def FormatSingleLine(*digits: list["BigChar"], spacing = 3) -> list[str]:
+    if not digits: return [] # Carefully avoid empty-state crashes
+    height = digits[0]._height
     separator = " " * spacing
-    resultLines = [""] * BigChar._height
-    for i in range(BigChar._height):
+    resultLines = [""] * height
+    for i in range(height):
       resultLines[i] += separator.join(map(lambda digit: digit._lines[i], digits))
     return resultLines
 
   @staticmethod
   def PrintSymbols(symbols: str) -> None:
-    bigChars = BigChar.FromSymbols(symbols)
-    lines = ["\n", *BigChar.FormatSingleLine(*bigChars)]
+    BigChar.PrintChars(BigChar.FromSymbols(symbols))
+
+  @staticmethod
+  def PrintChars(chars: list["BigChar"]) -> None:
+    lines = ["", *BigChar.FormatSingleLine(*chars), ""]
     print("\n".join(lines))
 
 
-  def __init__(self, chars: str, width = None):
+  def __init__(self, chars: str, width: int = None, height: int = 7):
     lines = chars.split("\n")
-    if len(lines)==9:
-      del lines[8]
+    if len(lines)==height+2:
       del lines[0]
-    if len(lines) != self._height:
-      raise ValueError("BigChar must be initialized with exactly 7 lines")
+      del lines[-1]
+    if len(lines) != height:
+      raise ValueError(f"BigChar must be initialized with exactly {height} lines")
 
+    self._height = height
     self._width = width if width else max(map(lambda line: len(line), lines))
     self._lines = list(map(lambda line: line.ljust(self._width), lines))
 
@@ -351,3 +357,27 @@ BIG_SYMBOLS = defaultdict(lambda: BIG_SYMBOLS["□"], map(parseCharText, [
 for digit in range(10):
   BIG_SYMBOLS[       digit ]=BIG_DIGITS[digit]
   BIG_SYMBOLS[""+str(digit)]=BIG_DIGITS[digit]
+
+class BigShades(BigChar):
+  Light = "░"
+  Medium = "▒"
+  Dark = "▓"
+  Fill = "█"
+
+
+  @staticmethod
+  def FromShading(shading: str) -> list["BigChar"]:
+    return list(map(lambda char: BIG_SHADING[char], shading))
+
+  @staticmethod
+  def PrintShades(shading: str) -> None:
+    BigChar.PrintChars(BigShades.FromShading(shading))
+
+def constructShadingChar(shading: str) -> BigChar:
+  shadingWidth = 4
+  line = shading * shadingWidth
+  shadingChars = line + "\n" + line
+  return shading, BigChar(shadingChars, height=2, width=shadingWidth)
+BIG_SHADING = dict(map(constructShadingChar, "█▓▒░"))
+
+BigShades.PrintShades("█▓▒░" + "".join(reversed("█▓▒░")))
