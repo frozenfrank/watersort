258faca2f573850531860f066901eaa4cb563b9c
James Finlinson <the30clues@gmail.com>
2025-12-26 15:04:01 -0700

    Avoid Fore.WHITE since it triggers special behaviors with system printing mechanisms for shading chars

--- resources.py
+++ resources.py
@@ -9,18 +9,18 @@ RESERVED_COLORS = set(["?", "-"])
 COLOR_CODES = defaultdict(str, {
   "bl": Back.BLACK + Fore.LIGHTWHITE_EX,          # Black (System)
   "m": Back.CYAN,                                 # Mint
-  "g": Back.LIGHTBLACK_EX + Fore.WHITE,           # Gray
+  "g": Back.LIGHTBLACK_EX + Fore.LIGHTWHITE_EX,   # Gray
   "gr": "",                                       # Green (Occasionally)
   "o": Back.YELLOW + Fore.RED,                    # Orange
   "y": Back.YELLOW + Fore.BLACK,                  # Yellow
-  "r": Back.RED + Fore.WHITE,                     # Red
+  "r": Back.RED + Fore.LIGHTWHITE_EX,             # Red
   "p": Back.BLACK  + Fore.MAGENTA,                # Purple
   "pk": Back.GREEN + Fore.BLACK,                  # Puke
   "pn": Back.MAGENTA,                             # Pink
   "br": Back.WHITE + Fore.MAGENTA,                # Brown
   "lb": Fore.CYAN + Back.WHITE,                   # Light Blue
   "gn": Back.BLACK + Fore.GREEN,                  # Dark Green
-  "b": Back.BLUE + Fore.WHITE,                    # Blue
+  "b": Back.BLUE + Fore.LIGHTWHITE_EX,            # Blue
   "?": "",                                        # Unknown
   "-": "",                                        # Empty
 })
