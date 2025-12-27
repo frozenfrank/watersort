frozenfrank/watersort

commit 9cc790e343ab34d9bf7bdce311f02600da51d007

Fix infinite loop when a solution cannot be found

watersort.py
@@ -1292,6 +1292,9 @@ def solveGame(game: "Game", solveMethod = "MIX", analyzeSampleCount = 0, probeDF
         numDeadEnds += 1
         deadEndDepth[current._numMoves] += 1
 
+    if not solution:
+      break  # There are no solutions
+
   # End timer
   endTime = time()
 

commit df879a26684f9c3cb2bffbed3d4311cfb2a3861f

Fix bug introduced in commit 9cc790e...
There are plenty of reasons that the search queue will abort a particular solution early.
We only want to alert the user when no solution has *ever* been found.

watersort.py
@@ -1391,7 +1391,7 @@ def solveGame(game: "Game", solveMethod = "MIX", analyzeSampleCount = 0, probeDF
         numDeadEnds += 1
         deadEndDepth[current._numMoves] += 1
 
-    if not solution:
+    if not minSolution:
       endTime = time()
       message = formatVialColor("er", "This game has no solution.")
       message += " Type YES if you have corrected the game state and want to try searching again."