84040557cdc003d8baa6bfe06cdd964c873d9422
James Finlinson <the30clues@gmail.com>
2026-01-25 18:15:50 -0600

    Disable references to Game while the colors are implemented

--- src/rust/core/mod.rs
+++ src/rust/core/mod.rs
@@ -1,7 +1,7 @@
 /// Core module exports
 
-pub mod game;
+// pub mod game;
 pub mod color;
 
-pub use game::Game;
+// pub use game::Game;
 pub use color::Color;
--- src/rust/main.rs
+++ src/rust/main.rs
@@ -1,18 +1,21 @@
 /// Main entry point for the Water Sort Puzzle CLI
 
-use watersort::Game;
-use watersort::types::Vial;
+// use watersort::Game;
+// use watersort::types::Vial;
 
 fn main() {
     println!("Water Sort Puzzle Solver - Rust Edition");
+    create_game();
+}
 
+fn create_game() {
     // Example: Create a simple game
-    let vial1: Vial = ['r', 'r', 'b', 'b'];
-    let vial2: Vial = ['g', 'g', 'y', 'y'];
-    let vial3: Vial = ['-', '-', '-', '-'];
+    // let vial1: Vial = ['r', 'r', 'b', 'b'];
+    // let vial2: Vial = ['g', 'g', 'y', 'y'];
+    // let vial3: Vial = ['-', '-', '-', '-'];
 
-    let game = Game::new_root(vec![vial1, vial2, vial3]);
+    // let game = Game::new_root(vec![vial1, vial2, vial3]);
 
-    println!("Game created with {} vials", game.num_vials());
-    println!("Game is finished: {}", game.is_finished());
+    // println!("Game created with {} vials", game.num_vials());
+    // println!("Game is finished: {}", game.is_finished());
 }
