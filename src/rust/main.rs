b48223ad1355639a86694ec6a9056ee80f2fbaf8
James Finlinson <the30clues@gmail.com>
2026-01-25 18:52:06 -0600

    Attempt to test ColorCodeAllocator (broken)

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -3,9 +3,52 @@
 // use watersort::Game;
 // use watersort::types::Vial;
 
+use watersort::core::{Color, ColorCodeAllocator, color::{EMPTY_SPACE, UNKNOWN_VALUE}};
+
 fn main() {
     println!("Water Sort Puzzle Solver - Rust Edition");
+    create_color_allocator();
     create_game();
+}
+
+fn create_color_allocator() {
+    let mut allocator = ColorCodeAllocator::new();
+
+    // Unknown Values
+    print_color_with_code_stats(&mut allocator, UNKNOWN_VALUE);
+    print_color_with_code_stats(&mut allocator, "?");
+    print_color_with_code_stats(&mut allocator, UNKNOWN_VALUE);
+
+    // Empty values
+    print_color_with_code_stats(&mut allocator, EMPTY_SPACE);
+    print_color_with_code_stats(&mut allocator, "-");
+    print_color_with_code_stats(&mut allocator, EMPTY_SPACE);
+
+    // Short color values
+    print_color_with_code_stats(&mut allocator, "b");
+    print_color_with_code_stats(&mut allocator, "lb");
+    print_color_with_code_stats(&mut allocator, "gn");
+    print_color_with_code_stats(&mut allocator, "m");
+    print_color_with_code_stats(&mut allocator, "y");
+    print_color_with_code_stats(&mut allocator, "b");
+
+    // Long color values
+    print_color_with_code_stats(&mut allocator, "blue");
+    print_color_with_code_stats(&mut allocator, "lightblue");
+    print_color_with_code_stats(&mut allocator, "green");
+    print_color_with_code_stats(&mut allocator, "mint");
+    print_color_with_code_stats(&mut allocator, "yellow");
+    print_color_with_code_stats(&mut allocator, "blue");
+
+    // Review standard colors
+    print_color_with_code_stats(&mut allocator, UNKNOWN_VALUE);
+    print_color_with_code_stats(&mut allocator, EMPTY_SPACE);
+}
+
+fn print_color_with_code_stats(allocator: &mut ColorCodeAllocator<'_>, color_name: &str) {
+    let code = &Color(color_name);
+    let color_code = allocator.assign_code(code);
+    println!("Color Code [{} = {}, code = {:p}, str = {:p}]", color_name, color_code, code, code.0);
 }
 
 fn create_game() {
