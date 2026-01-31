a60efd776ba3de5e7a3dac6e6bed3258b335e023
James Finlinson <the30clues@gmail.com>
2026-01-31 16:50:29 -0600

    Reenable Game connections (broken)

--- src/rust/core/mod.rs
+++ src/rust/core/mod.rs
@@ -1,11 +1,11 @@
 /// Core module exports
 
-// pub mod game;
+pub mod game;
 pub mod color;
 pub mod color_code;
 pub mod color_code_demo;
 
-// pub use game::Game;
+pub use game::Game;
 pub use color::Color;
 pub use color_code::{ColorCode, ColorCodeAllocator};
 pub use color_code_demo::run_color_allocator_debug;
