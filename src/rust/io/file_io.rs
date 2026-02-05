d5b2e1d6ff93758468767c9da1f49344fd648f7a
James Finlinson <the30clues@gmail.com>
2026-02-04 23:41:37 -0600

    Begin updating file_io.rs file

--- src/rust/io/file_io.rs
+++ src/rust/io/file_io.rs
@@ -1,12 +1,17 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufWriter, Write};
 use crate::core::Game;
+use crate::core::game_settings::GameSettings;
 
 pub fn save_game(game: &std::sync::Arc<Game>, path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::create(path)?;
-    writeln!(file, "i")?;
+    let file = File::create(path)?;
+    let mut writer = BufWriter::new(file);
+
+    writeln!(&mut writer, "i")?;
     let settings = *game.settings.borrow();
-    let level_line = format!("{} {}", settings.level, settings.special_modes.join(" "));
+
+    let special_modes = get_special_modes(&settings);
+    let level_line = format!("{} {}", settings.level, special_modes.join(" "));
     writeln!(file, "{}", level_line.trim())?;
     writeln!(file, "{}", game.num_vials())?;
     for vial in &settings.original_vials {
@@ -14,4 +19,20 @@ pub fn save_game(game: &std::sync::Arc<Game>, path: &str) -> Result<(), Box<dyn 
         writeln!(file, "{}", line)?;
     }
     Ok(())
+}
+
+fn get_special_modes(settings: &GameSettings) -> Vec<&str> {
+    let mut modes = Vec::<&str>::new();
+
+    if settings.drain_mode {
+        modes.push(&"drain");
+    }
+    if settings.blind_mode {
+        modes.push(&"blind");
+    }
+    if settings.had_mystery_spaces {
+        modes.push(&"mystery");
+    }
+
+    modes
 }
