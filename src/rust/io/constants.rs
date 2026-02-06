411444f9f1a1c24449255bcea0119e7ac63b8d73
James Finlinson <the30clues@gmail.com>
2026-02-05 22:23:27 -0600

    Commit file_io constants

---
+++ src/rust/io/constants.rs
@@ -0,0 +1,15 @@
+
+// Configuration constants
+pub const WRITE_FILES_TO_ABSOLUTE_PATH: bool = false;
+pub const INSTALLED_BASE_PATH: &str = "";
+
+// Level storage constants
+pub const LEVEL_DIR: &str = "wslevels";
+pub const LEVEL_EXT: &str = "txt";
+
+pub const MONTH_ABBRS: &[&str] = &["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+pub const DRAIN_MODE_FLAG: &str = "drain";
+pub const DRAIN_MODE_FLAG_2: &str = "pour";
+pub const BLIND_MODE_FLAG: &str = "blind";
+pub const MYSTERY_MODE_FLAG: &str = "mystery";
