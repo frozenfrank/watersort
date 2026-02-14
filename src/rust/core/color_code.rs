fe78849c22f03ef7446ce5105cd4e23401ced942
James Finlinson <the30clues@gmail.com>
2026-02-14 16:15:05 -0600

    Extract ColorCodeAllocator to separate file

--- src/rust/core/color_code.rs
+++ src/rust/core/color_code.rs
@@ -1,10 +1,3 @@
-use std::{collections::HashMap, rc::Rc};
-
-use crate::core::{
-    Color,
-    color::{EMPTY_SPACE, UNKNOWN_VALUE},
-};
-
 pub type ColorCode = u8;
 
 pub const COLOR_CODE_EMPTY: ColorCode = 0;
@@ -21,67 +14,5 @@ impl ColorCodeExt for ColorCode {
     }
     fn is_unknown(self) -> bool {
         self == COLOR_CODE_UNKNOWN
-    }
-}
-
-#[derive(Clone, PartialEq, Eq)]
-pub struct ColorCodeAllocator {
-    color_codes: HashMap<Rc<Color>, ColorCode>,
-    colors: Vec<Rc<Color>>,
-}
-
-impl ColorCodeAllocator {
-    pub fn new() -> Self {
-        let mut allocator = Self {
-            color_codes: HashMap::new(),
-            colors: Vec::new(),
-        };
-
-        // Reserve 0 and 1 for EMPTY_SPACE and UNKNOWN_COLOR
-        allocator.allocate_color(&Color::new(EMPTY_SPACE));
-        allocator.allocate_color(&Color::new(UNKNOWN_VALUE));
-        allocator
-    }
-
-    fn allocate_color(&mut self, color: &Color) -> ColorCode {
-        let assigned_code = self.colors.len() as ColorCode;
-        let boxed_color = Rc::new(color.clone());
-        self.color_codes.insert(boxed_color.clone(), assigned_code);
-        self.colors.push(boxed_color);
-        assigned_code
-    }
-
-    pub fn assign_code(&mut self, color: &Color) -> ColorCode {
-        // CONSIDER: Performing a linear search through self.colors while the array is small.
-        // This could be cheaper since we expect the total number of colors to be normally small.
-        if let Some(&code) = self.color_codes.get(color) {
-            code
-        } else {
-            self.allocate_color(&color)
-        }
-    }
-
-    pub fn interpret_code(&self, code: ColorCode) -> Rc<Color> {
-        self.colors[code as usize].clone()
-    }
-
-    pub fn num_colors(&self) -> usize {
-        self.colors.len() - 2 // Subtract 2 for EMPTY_SPACE and UNKNOWN_COLOR
-    }
-}
-
-impl std::fmt::Debug for ColorCodeAllocator {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("ColorCodeAllocator")
-            // .field("color_codes", &self.color_codes)
-            .field(
-                "colors",
-                &self
-                    .colors
-                    .iter()
-                    .map(|color| &color.key)
-                    .collect::<Vec<_>>(),
-            )
-            .finish()
     }
 }
---
+++ src/rust/core/color_code_allocator.rs
@@ -0,0 +1,67 @@
+use crate::core::{
+    Color, ColorCode,
+    color::{EMPTY_SPACE, UNKNOWN_VALUE},
+};
+use std::{collections::HashMap, rc::Rc};
+
+#[derive(Clone, PartialEq, Eq)]
+pub struct ColorCodeAllocator {
+    color_codes: HashMap<Rc<Color>, ColorCode>,
+    colors: Vec<Rc<Color>>,
+}
+
+impl ColorCodeAllocator {
+    pub fn new() -> Self {
+        let mut allocator = Self {
+            color_codes: HashMap::new(),
+            colors: Vec::new(),
+        };
+
+        // Reserve 0 and 1 for EMPTY_SPACE and UNKNOWN_COLOR
+        allocator.allocate_color(&Color::new(EMPTY_SPACE));
+        allocator.allocate_color(&Color::new(UNKNOWN_VALUE));
+        allocator
+    }
+
+    fn allocate_color(&mut self, color: &Color) -> ColorCode {
+        let assigned_code = self.colors.len() as ColorCode;
+        let boxed_color = Rc::new(color.clone());
+        self.color_codes.insert(boxed_color.clone(), assigned_code);
+        self.colors.push(boxed_color);
+        assigned_code
+    }
+
+    pub fn assign_code(&mut self, color: &Color) -> ColorCode {
+        // CONSIDER: Performing a linear search through self.colors while the array is small.
+        // This could be cheaper since we expect the total number of colors to be normally small.
+        if let Some(&code) = self.color_codes.get(color) {
+            code
+        } else {
+            self.allocate_color(&color)
+        }
+    }
+
+    pub fn interpret_code(&self, code: ColorCode) -> Rc<Color> {
+        self.colors[code as usize].clone()
+    }
+
+    pub fn num_colors(&self) -> usize {
+        self.colors.len() - 2 // Subtract 2 for EMPTY_SPACE and UNKNOWN_COLOR
+    }
+}
+
+impl std::fmt::Debug for ColorCodeAllocator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ColorCodeAllocator")
+            // .field("color_codes", &self.color_codes)
+            .field(
+                "colors",
+                &self
+                    .colors
+                    .iter()
+                    .map(|color| &color.key)
+                    .collect::<Vec<_>>(),
+            )
+            .finish()
+    }
+}
