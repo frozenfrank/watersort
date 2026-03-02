115fb2121c25228aebd42d27e488c79e964f6f8f
James Finlinson <the30clues@gmail.com>
2026-03-01 23:26:44 -0600

    AMEND: 307a6aa Combine duplicative solver code into GenericSolver

--- src/rust/solver/analysis/analysis_strategy.rs
+++ src/rust/solver/analysis/analysis_strategy.rs
@@ -1,7 +1,20 @@
-use crate::solver::strategy::SolverStrategy;
+use crate::solver::{analysis::MoveDepthCounter, strategy::SolverStrategy};
 
 pub struct AnalysisStrategy {
 
+}
+
+pub struct AnalysisSummaryData {
+    pub partial_depth: MoveDepthCounter,
+    pub dup_game_depth: MoveDepthCounter,
+    pub swallowed_depth: MoveDepthCounter,
+    pub dead_end_depth: MoveDepthCounter,
+    pub solution_depth: MoveDepthCounter,
+    pub unique_sols_depth: MoveDepthCounter,
+    // Not yet represented
+    // pub sol_find_seconds: bool,
+    // pub unique_solutions: bool,
+    // pub is_unique_list: bool,
 }
 
 impl SolverStrategy for AnalysisStrategy {
